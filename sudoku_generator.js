8557b2942f7e2b825124cf471cfface69915a650
Pixou2000 <7monmel@gmail.com>
2026-04-01 10:40:18 +0200

    Add files via upload

--- sudoku_generator.js
+++ sudoku_generator.js
@@ -126,7 +126,7 @@ function generateSudoku(cellsToRemove = 45) {
 function generateSudokuByLevel(niveauVise, essaisMax) {
     const retraitParNiveau = {
         facile: [32, 33, 34, 35, 36, 37, 38],
-        moyen: [40, 41, 42, 42, 43, 43, 44],
+        moyen: [44, 45, 45, 46, 46],
         difficile: [45, 46, 47, 48, 49, 50, 51],
         expert: [50, 51, 52, 53, 54, 55]
     };
